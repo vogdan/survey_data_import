c76864fe951c1503d8f59f1950edef5f23f6ac05
“Bogdan” <dbogdan@gmail.com>
2013-12-13 15:51:32 +0200

    added oop structure

--- surveyDI.py
+++ surveyDI.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 from argparse import ArgumentParser
 from time import gmtime, strftime
 import os
@@ -9,15 +11,15 @@ from surveyDI_conf import OPUT_QR, OPUT_SQ, OPUT_Q, OPUT_R, OPUT_S
 def parse_cli_opts():
     global args
 
-    parser = ArgumentParser(description='''convert the CSV files in the input\ 
+    arg_parser = ArgumentParser(description='''convert the CSV files in the input\ 
 directory to a format that can be easily be imported into a database''')
-    parser.add_argument('-i', '--input_dir', 
+    arg_parser.add_argument('-i', '--input_dir', 
                         help='Directory containing input csv files', 
                         required=True)
-    parser.add_argument('-o', '--output_dir', 
+    arg_parser.add_argument('-o', '--output_dir', 
                         help='Directory that will contain output files', 
                         default=".")
-    args = parser.parse_args()
+    args = arg_parser.parse_args()
 
 
 def main():
@@ -25,9 +27,12 @@ def main():
 
     parse_cli_opts()
     output_file = 'Surveys.tab'
-    surveyDI_lib.get_surveys(args.input_dir, output_file)
-    output_file = 'Questions.tab'
-    surveyDI_lib.get_questions(args.input_dir, output_file)
+    file_parser = surveyDI_lib.Parser(args.input_dir)
+    file_parser.write_surveys(output_file)
+
+#    surveyDI_lib.get_surveys(args.input_dir, output_file)
+#    output_file = 'Questions.tab'
+#    surveyDI_lib.get_questions(args.input_dir, output_file)
 
 if __name__ == "__main__":
     log_delimiter = "#"*20 + strftime("%a, %d %b %Y %X +0000", gmtime()) + "#"*10
--- surveyDI_lib.py
+++ surveyDI_lib.py
@@ -2,48 +2,49 @@ import csv
 from surveyDI_conf import logger
 import os
 
+
 def get_csv_files(input_dir, suffix=".csv"):
     """
     Scan the input directory and return a list containing all the .csv files
     found there.
-
+    
     :input: a directory containing input files
             suffix of the input files (by default set to ".csv")
     :return: list containing the csv files in input_dir (full path) 
     """
-
+    
     filenames = os.listdir(input_dir)
     return [os.path.join(input_dir, filename) for filename in filenames 
             if filename.endswith(suffix)]
 
-
 def read_csv2dict(input_file):
     """
     Read a csv file in a csv reader dictionary
-
+    
     :input: input_file - csv input file
     :return: the csv reader object
     """
+    
     logger.info("Reading input file {}".format(input_file))
     with open(input_file) as csv_file:
         reader = csv.DictReader(csv_file)
-
     return reader
 
 
 def write_to_csv(output_file, headers, values_list, delim='\t'):
     """
     Write header and corresponding values to a csv file
-
+        
     :input: desired output file name
-           a list representing the csv file header
-           a list of lists, each representing the corresponding values
+            a list representing the csv file header
+            a list of lists, each representing the corresponding values
     :return: nothing
     :calling example:
-              # write webinar details to csv
-              w_info = get_webinar_info(INPUT_FILE)
-              write_to_csv(OUTPUT_WEBINARS, w_info[0], [w_info[1]])
+    # write webinar details to csv
+            w_info = get_webinar_info(INPUT_FILE)
+            write_to_csv(OUTPUT_WEBINARS, w_info[0], [w_info[1]])
     """
+
     logger.info("\tWriting file {}...".format(output_file))
     with open(output_file, 'wb') as csv_file:
         writer = csv.writer(csv_file, delimiter=delim)
@@ -52,37 +53,80 @@ def write_to_csv(output_file, headers, values_list, delim='\t'):
             writer.writerow(values)
 
 
-def get_surveys(input_dir, output_file):    
-    header = ["SurveyID", "Filename"]
-    file_list = [(i+1,x) for i,x in enumerate(get_csv_files(input_dir))]
-    write_to_csv(output_file, header, file_list)
+
+class InputFile():
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+    def get_id(self):
+        return self.id
+
+    def get_filename(self):
+        return os.path.basename(self.name)
+
+    def __str__(self):
+        print "{} {}".format(self.id, self.name)
 
 
-def get_questions(input_dir, output_file):
-    """
-    Get info for the "Questions.tab" output file
+class Question():
+    def __init__(self, id, text, fileid):
+        self.id = id
+        self.text = text
+        self.fileid = []
+
+    def set_fieldid(self, fileid):
+        self.fileid.append(fileid)
+
+
+class Parser():
+    def __init__(self, input_dir):
+        self.input_dir = input_dir
+        #surveys
+        self.surveys = []
+        self.fheader = ["SurveyID", "Filename"] 
+        
+        #questions
+        self.questions = []
+        self.qheader = ["QuestionID", "QuestionText"]
+
+    def get_surveys(self):
+        survey_list = get_csv_files(self.input_dir)
+        self.surveys = [InputFile(id+1, name) for id, name in enumerate(survey_list)]
+        
+    def get_questions(self):
+        """
+        Get info for the "Questions.tab" output file
+        
+        :input: input_files - list of InputFile instances
+                output_file - the name of the file containing the questions
+        :output: 
+        """
+        if not self.input_files:
+            get_inputfiles()
+        for input_file in self.input_files:
+            questions_delim = "Custom Data"
+            try:
+                with open(input_file.name, 'rb') as csv_file:
+                    file_id = input_file.get_id()
+                    reader = csv.reader(csv_file)
+                    headers = reader.next()
+                    qstart_idx = headers.index(questions_delim) + 1
+                    questions_list.extend([x for x in headers[qstart_idx:] 
+                                      if x not in questions_list])
+            except IOError as e:
+                logger.error("Cannot read file '{}'".format(input_file))
+                logger.debug("Exception:\n{}".format(e))
+        self.questions = [Question(id, text, file_id) for id, text in enumerate(questions_list)]
+
+    def write_surveys(self, output_file):
+        if not self.surveys:
+            self.get_surveys()
+        survey_list = [(survey.id, survey.get_filename()) for survey in self.surveys]
+        write_to_csv(output_file, self.fheader, survey_list)
     
-    :input: input_dir - Directory containing input files
-    :output: A set containing the questions from all input files (same 
-             question in different files will appear onlu once here)
-    """
+    def write_questions(self):
+        questions_list = [(question.id, question.text) for question in self.questions]
+        write_to_csv(output_file, header, questions_list)
+    
 
-    questions = []
-
-    for input_file in get_csv_files(input_dir):
-        questions_delim = "Custom Data"
-        output_header = ["QuestionID", "QuestionText"]
-        try: 
-            with open(input_file, 'rb') as csv_file:
-                reader = csv.reader(csv_file)
-                headers = reader.next()
-                qstart_idx = headers.index(questions_delim) + 1
-                questions.extend([x for x in headers[qstart_idx:] 
-                                  if x not in questions])
-        except IOError as e:
-            logger.error("Cannot read file '{}'".format(input_file))
-            logger.debug("Exception:\n{}".format(e))
-
-    write_to_csv(output_file, 
-                 output_header, 
-                 [(i,item) for i,item in enumerate(questions)])
